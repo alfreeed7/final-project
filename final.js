af01bef1cd5d16ecfd8e7d60416481ac19b21ab8
Hao Zhong <alfred.zhong@centre.edu>
2019-11-25 19:02:13 -0500

    hhh

--- final.js
+++ final.js
@@ -1,21 +1,32 @@
+
 mappromise=d3.json("world-countries.json")
 datapromise=d3.csv("final.csv")
 
-mappromise.then
-(function(country)
-{
-   console.log("country",country)
-    drawmap(country)
+
+Promise.all([mappromise, datapromise])
+       .then(function(values)
+       {
+         var  all= values[0];
+         var economics = values[1];
+    
+    console.log("all", all)
+    
+      var getdata = {}
+
+  economics.forEach(function(element)
+         {
+           getdata[element.countrycode] = element; 
+         })
+    
+   all.features.forEach(function(feature)
+         {
+           feature.properties.economicdata = getdata[feature.properties.iso_a3];
+         })
+
+    drawmap(all)
 })
 
-datapromise.then
-(
-function(stat)
-    {console.log("stat",stat)}
-)
-
-
-var drawmap=function(country) 
+var drawmap=function(all) 
 {
 var screen = {width: 1500, height:1000}
 
@@ -31,10 +42,47 @@ var path=d3.geoPath(projection)
               .attr("height", screen.height);
 
     svg.selectAll("path")
-    .data(country.features)
+    .data(all.features)
     .enter()
     .append("path")
     .attr("d",path)
+    .attr("stroke", "#000000")
+    
+    .style("fill", function(d) {
+
+var value = d.properties.economicdata.rgdppercapita17;
+if (value) 
+{
+return color(value);
+} 
+        else 
+        {
+return "#ccc";
+}   
+})
+
+
 }
+
+var color = d3.scaleQuantize()
+.range(["#edf8fb", "#ccece6",
+"#99d8c9", "#66c2a4", "#2ca25f","#006d2c"])
+.domain([0,30000])
+
+var button1=
+    d3.select("body")
+.append("button")
+.attr("class","button1")
+.text("Based on Real GDP per capita")
+
+var button2=
+        d3.select("body")
+.append("button")
+.attr("class","button2")
+.text("Based on Capital per capita")
+
+
+
+
 
 
