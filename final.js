3e290954e0a45bd17b1cff47e58a02425256e8e9
Alfred Zhong <alfred.zhong@centre.edu>
2019-12-04 19:04:25 -0500

    hello

--- final.js
+++ final.js
@@ -24,6 +24,7 @@ Promise.all([mappromise, datapromise])
     button1(all)
     button2(all)
     button3(all)
+
 })
 
 var drawmap=function(all) 
@@ -47,8 +48,46 @@ var path=d3.geoPath(projection)
     .enter()
     .append("path")
     .attr("d",path)
-    .attr("stroke", "#000000")
+    .style("fill","white")
+}
+
+var button1=function(all)
+{
+
     
+    d3.select("body")
+.append("button")
+.attr("class","button1")
+.text("Based on Real GDP per capita")
+.on("click",function()
+    
+   {
+        
+        d3.selectAll("p")
+        .remove()
+        
+          d3.select(".svg1")
+        .remove()
+        
+        d3.select(".svg2")
+        .remove()
+        
+              d3.select(".svg3")
+        .remove()
+        
+        d3.select(".legend")
+    .remove()
+    
+d3.select(".compare")
+        .remove()
+        
+        
+        drawmap(all)
+        
+        d3.selectAll("path")
+   .transition()
+        .duration(1000)
+   .attr("stroke", "#000000") 
     .style("fill", function(d) {
 
 var value = d.properties.economicdata.rgdppercapita17;
@@ -62,10 +101,27 @@ return "#ccc";
 }   
 })
 
+        
+    drawlegend()
+
+})
+}
+
+var color = d3.scaleQuantize()
+.range(["#edf8fb", "#ccece6",
+"#99d8c9", "#66c2a4", "#2ca25f","#006d2c"])
+.domain([0,30000])
+
+var drawlegend=function()
+
+{
+
+
   var colorSet = ["#edf8fb", "#ccece6", "#99d8c9", "#66c2a4", "#2ca25f","#006d2c","#ccc"];
   var description = ["0-$5000","$5000-$10000","$10000-$15000","$15000-$20000", "$20000-$25000", ">$25000 (developed countries)","Undefined"];    
     
-      var legend = svg.append("g")
+      var legend =d3.select(".svg1")
+          .append("g")
                   .attr("class", "legend")
 
   legend.selectAll("rect")
@@ -88,37 +144,44 @@ return "#ccc";
         .attr("font-size", 15)
         .attr("font-style", "italic")
         .attr("font-weight", "bold");
-
 }
 
-var color = d3.scaleQuantize()
-.range(["#edf8fb", "#ccece6",
-"#99d8c9", "#66c2a4", "#2ca25f","#006d2c"])
-.domain([0,30000])
+var button2=function(all)
 
-
-var drawmap2=function(all) 
 {
-var screen = {width: 1500, height:1000}
-
- var projection = d3.geoEqualEarth()
-                     .translate([screen.width/2, screen.height/2])
-                     .scale([300]);
-
-var path=d3.geoPath(projection)
-
-  var svg = d3.select("body")
-              .append("svg")
-  .attr("class","svg1")
-              .attr("width", screen.width)
-              .attr("height", screen.height);
-
-    svg.selectAll("path")
-    .data(all.features)
-    .enter()
-    .append("path")
-    .attr("d",path)
-    .attr("stroke", "#000000")
+    
+    
+    d3.select("body")
+.append("button")
+.attr("class","button2")
+.text("Growth of Real GDP per Capita/Growth of Capital per capita")
+.on("click",function()
+   {
+                d3.selectAll("p")
+        .remove()      
+        
+//          d3.select(".svg1")
+//        .remove()
+        
+        d3.select(".svg2")
+        .remove()
+        
+              d3.select(".svg3")
+        .remove()
+        
+        d3.select(".legend")
+    .remove()
+    
+d3.select(".compare")
+        .remove()
+        
+    //        drawmap(all)
+        
+        
+        d3.selectAll("path")
+   .transition()
+        .duration(1000)
+   .attr("stroke", "#000000")
     
     .style("fill", function(d) {
 
@@ -133,10 +196,26 @@ return "#ccc";
 }   
 })
 
+
+    drawlegend2()
+
+})}
+
+
+    var color2 = d3.scaleQuantize()
+.range(["#edf8fb", "#ccece6",
+"#99d8c9", "#66c2a4", "#2ca25f","#006d2c"])
+.domain([0,1.2])
+
+    var drawlegend2=function()
+
+{
+    
     var colorSet2 = ["#edf8fb", "#ccece6", "#99d8c9", "#66c2a4", "#2ca25f","#006d2c","#ccc"];
   var description2 = ["<0.2","0.2-0.4","0.4-0.6","0.6-0.8", "0.8-1", ">1","Undefined"];  
           
-    var legend = svg.append("g")
+
+        var legend = d3.select(".svg1").append("g")
                   .attr("class", "legend")
     
       legend.selectAll("rect")
@@ -159,35 +238,11 @@ return "#ccc";
         .attr("font-size", 15)
         .attr("font-style", "italic")
         .attr("font-weight", "bold");
+    
 
 }
 
-var color2 = d3.scaleQuantize()
-.range(["#edf8fb", "#ccece6",
-"#99d8c9", "#66c2a4", "#2ca25f","#006d2c"])
-.domain([0,1.2])
-
-
-var button1=function(all)
-{
-    d3.select("body")
-.append("button")
-.attr("class","button1")
-.text("Based on Real GDP per capita")
-.on("click",function()
-   {removethings(),drawmap(all)})
-}
-
-
-var button2=function(all)
-{d3.select("body")
-.append("button")
-.attr("class","button2")
-.text("Growth of Real GDP per Capita/Growth of Capital per capita")
-.on("click",function()
-   {removethings(),drawmap2(all)})}
-
-var button3=function(all)
+    var button3=function(all)
 {
         d3.select("body")
 .append("button")
@@ -442,6 +497,8 @@ d3.select(".svg3")
     .append("line")
     .attr("x1",50)
     .attr("y1",460)
+     .transition()
+  .duration(1500)
     .attr("x2",800)
     .attr("y2",40)
     .attr("stroke-width", 3)
@@ -532,6 +589,8 @@ d3.select("body")
     .append("line")
     .attr("x1",50)
     .attr("y1",400)
+         .transition()
+  .duration(1500)
     .attr("x2",800)
     .attr("y2",130)
     .attr("stroke-width", 3)
@@ -619,6 +678,8 @@ d3.select("body")
     .append("line")
     .attr("x1",50)
     .attr("y1",330)
+         .transition()
+  .duration(1500)
     .attr("x2",800)
     .attr("y2",130)
     .attr("stroke-width", 3)
